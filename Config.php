31b8e29988cab24a73a99ea9eec56ad112bc9b02
Abhishek Kumkar <abhishekkumkar@gmail.com>
2020-09-25 11:12:39 +0530

    removed successful con line

--- Config.php
+++ Config.php
@@ -4,5 +4,5 @@
    define('DB_PASSWORD', 'fqubDnbQER');
    define('DB_DATABASE', 'sql12367278');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   echo "successful connection"
+//    echo "successful connection"
 ?>
