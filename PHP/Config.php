833b3f5d712c7121c5d93c998d46429ac5f55ca9
Abhishek Kumkar <45125059+abhishekkumkar@users.noreply.github.com>
2020-11-12 22:18:57 +0530

    Update Config.php

--- PHP/Config.php
+++ PHP/Config.php
@@ -1,8 +1,8 @@
 <?php
    define('DB_SERVER', 'sql12.freemysqlhosting.net');
-   define('DB_USERNAME', 'sql12373536');
-   define('DB_PASSWORD', 'XgLXTEBBVg');
-   define('DB_DATABASE', 'sql12373536');
+   define('DB_USERNAME', 'sql12376199');
+   define('DB_PASSWORD', 'zbmFuHNsDn');
+   define('DB_DATABASE', 'sql12376199');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 //    echo "successful connection"
 ?>
